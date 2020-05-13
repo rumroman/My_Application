f6677e5039420bdccb929ecb4a2119f9765fbc64
Rum <romanrum45@gmail.com>
2020-05-14 03:11:35 +0500

    init

--- app/src/main/java/com/example/myapplication/Search.java
+++ app/src/main/java/com/example/myapplication/Search.java
@@ -3,12 +3,56 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
 
 public class Search extends AppCompatActivity {
+
+    SearchView mySearchView;
+    ListView myList;
+
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        mySearchView = (SearchView)findViewById(R.id.searchView);
+        myList = (ListView)findViewById(R.id.myList);
+
+        list = new ArrayList<>();
+
+        list.add("Monday");
+        list.add("Tuesday");
+        list.add("Wednesday");
+        list.add("Thursday");
+        list.add("Friday");
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.myList, list);
+
+        myList.setAdapter(adapter);
+
+        mySearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+
+                adapter.getFilter().filter(s);
+
+
+                return false;
+            }
+        });
+
     }
+
 }
