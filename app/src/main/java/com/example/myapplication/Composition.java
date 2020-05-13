df16393e4105c98252c6d4a6fb356a8985df6c85
Rum <romanrum45@gmail.com>
2020-05-14 02:04:30 +0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Composition.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Composition extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_composition);
+
+    }
+
+
+}
